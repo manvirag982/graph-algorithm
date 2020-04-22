7798041c7e334ec51d4fcf1d39ff8b80c152031d
manvirag <anubhavguptadec1999@gmail.com>
2020-04-22 11:59:21 +0530

    Create unweighted_shortest_path.cpp

---
+++ unweighted_shortest_path.cpp
@@ -0,0 +1,38 @@
+We write code for the described algorithm in C++.
+
+vector<vector<int>> adj;  // adjacency list representation
+int n; // number of nodes
+int s; // source vertex
+
+queue<int> q;
+vector<bool> used(n);
+vector<int> d(n), p(n);
+
+q.push(s);
+used[s] = true;
+p[s] = -1;
+while (!q.empty()) {
+    int v = q.front();
+    q.pop();
+    for (int u : adj[v]) {
+        if (!used[u]) {
+            used[u] = true;
+            q.push(u);
+            d[u] = d[v] + 1;
+            p[u] = v;      // if no printing no need to store parent
+        }
+    }
+}
+If we have to restore and display the shortest path from the source to some vertex u, it can be done in the following manner:
+
+if (!used[u]) {
+    cout << "No path!";
+} else {
+    vector<int> path;
+    for (int v = u; v != -1; v = p[v])
+        path.push_back(v);
+    reverse(path.begin(), path.end());
+    cout << "Path: ";
+    for (int v : path)
+        cout << v << " ";
+}
