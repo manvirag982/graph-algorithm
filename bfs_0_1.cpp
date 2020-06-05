87faa9e1e067cbb795179130f928e607a2dfd4b5
manvirag <anubhavguptadec1999@gmail.com>
2020-06-05 16:37:51 +0530

    Update bfs_0_1.cpp

--- bfs_0_1.cpp
+++ bfs_0_1.cpp
@@ -1,3 +1,16 @@
+/*
+    my logic
+    since priority queue application -> to give shortest distance node first
+    now if i get only two value then we can sort it in constant time by putting smaller in front and bigger in back
+    see example
+    if weight 0,x
+    suppose in pq--> 0,0,x,x
+    if two option after relation 0+0 or 0+x ---> in pq we push 0+x--> x still two element in pq
+    or suppose pq--> x,x,x
+    x+0,x+x--> pq--> x,x,2x still two element and back at weight of x
+               
+*/
+
 vector<int> d(n, INF);
 d[s] = 0;
 deque<int> q;
